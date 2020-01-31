f4e3b91ff211a8bda3944f555e2b21d532a66071
Szörfi Jázmin <jazmin.szorfi@gmail.com>
2020-01-31 23:52:40 +0100

    Script folder

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Graph {
+
+    private static List<Node> nodes = new List<Node>();
+
+    public static List<Node> findPathToNew(Node from)
+    {
+        var path = new List<Node>();
+        return path;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    public bool visited = false;
+    private Dictionary<Node, Vector2> neighbours = new Dictionary<Node, Vector2>();
+
+    public Vector2 findDoor()
+    {
+        foreach (var n in neighbours)
+        {
+            if (!n.Key.visited)
+                return n.Value;
+        }
+
+
+    }
+}
