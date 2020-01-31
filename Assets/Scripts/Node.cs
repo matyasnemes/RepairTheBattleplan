44bb705b1f6b10bde22a2aa18d9cc2edb4104c0c
Szörfi Jázmin <jazmin.szorfi@gmail.com>
2020-02-01 00:06:32 +0100

    Scripts again

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node {
+public class Node
+{
     public bool visited = false;
     private Dictionary<Node, Vector2> neighbours = new Dictionary<Node, Vector2>();
 
@@ -14,6 +15,6 @@ public class Node {
                 return n.Value;
         }
 
-
+        Graph.findPathToNew(this);
     }
 }
