0668715734737e7bd31d46b73c034338a6ae6e47
Peterskhan <peterskhan1997@gmail.com>
2020-02-01 14:57:32 +0100

    Slightly more advanced GUI

---
+++ Assets/GUI/GUIScripts/ActionButtonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ActionButtonController :   MonoBehaviour, 
+                                        IPointerEnterHandler,
+                                        IPointerExitHandler,
+                                        IPointerClickHandler
+{
+    public static int currentAction;
+    public Image tooltipImage;
+
+    public void Start()
+    {
+        tooltipImage.enabled = false;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tooltipImage.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tooltipImage.enabled = false;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Fuckyou more!");
+    }
+
+}
