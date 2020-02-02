0015043102e68210ad499a128cf2332f9610ae77
Kertész Gergő <kerteszcsgergo@gmail.com>
2020-02-02 17:16:39 +0100

    Peti bazdmeg

--- Assets/Scripts/GeneratorEntity.cs
+++ Assets/Scripts/GeneratorEntity.cs
@@ -23,6 +23,10 @@ public class GeneratorEntity : MonoBehaviour
     public GameObject StartTile;
     public GameObject FinishTile;
     public GameObject GoblinPrefab;
+    public GameObject c1100;
+    public GameObject c0110;
+    public GameObject c0011;
+    public GameObject c1001;
 
     public List<GameObject> RoomObjects;
     public List<GameObject> CorridorObjects;
