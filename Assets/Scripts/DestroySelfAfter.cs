a73d45b31ecd11e73ea4f8df25dd89e2cc0f4c37
BuildTools <matyasnemes@gmail.com>
2020-02-02 13:00:20 +0100

    effectprefabs

---
+++ Assets/Scripts/DestroySelfAfter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelfAfter : MonoBehaviour
+{
+
+    private float time;
+
+    public float timeToLive = 0.6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > time + timeToLive)
+        {
+            Destroy(this.gameObject);
+        }   
+    }
+}
