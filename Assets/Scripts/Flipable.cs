7558cd5ee627bc0eaf3aed648589f954b2903a14
Szörfi Jázmin <jazmin.szorfi@gmail.com>
2020-02-04 00:08:24 +0100

    Flipable script

---
+++ Assets/Scripts/Flipable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipable : MonoBehaviour
+{
+    private Vector3 oldPosition;
+    public Vector3 referenceAxis;
+    void Start()
+    {
+        referenceAxis = new Vector3(1, 0, 0);
+        oldPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = Vector3.Dot(transform.position - oldPosition, referenceAxis);
+        oldPosition = transform.position;
+        if (direction >= 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
